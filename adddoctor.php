aec9c11b63744d21d8bd98c08e92693a93292e51
Christine Richards <cricha88@uwo.ca>
2019-11-17 23:36:16 -0500

    Ending insert doctor

--- adddoctor.php
+++ adddoctor.php
@@ -8,12 +8,12 @@ $spec = $_POST["Specialty"];
 $licdate = $_POST["LicenseDate"];
 $hospcode = $_POST["whichhosp"];
 print_r($hospcode);
-//$query= 'INSERT INTO workofart (artname, artist, year, whichmus) VALUES ("' .
-//$art .'","' . $artist . '","' . $theyear . '","' . $whichmus . '");';
-//if (!mysqli_query($connection,$query)) {
-//	die ("Error while trying to add new doctor.". mysqli_error($connection));
-//} else {
-//	header('Location: index.php'); //send back to index page once it is done
-//	exit;
-//}
+$query= 'INSERT INTO doctor (LicenseNumber, FirstName, LastName, Specialty, LicenseDate, HospitalCode) VALUES ("' .
+$licnum .'","' . $fname . '","' . $lname . '","' . $spec . '","' . $licdate . '","' . $hospcode . '");';
+if (!mysqli_query($connection, $query)) {
+	die ("Error while trying to add new doctor.". mysqli_error($connection));
+} else {
+	header('Location: index.php'); //send back to index page once it is done
+	exit;
+}
 ?>
--- gethospital.php
+++ gethospital.php
@@ -1,13 +1,14 @@
 <?php
 	$query = "SELECT * FROM hospital";
 	$result = mysqli_query($connection,$query);
-   	if (!$result) {
+    echo "New Doctor's Hospital Name: </br>";
+   	
+    if (!$result) {
         die("databases query for hospitals failed.");
     }
-    echo "New Doctor's Hospital Name: </br>";
     while ($row = mysqli_fetch_assoc($result)) {
       echo "<option value='".$row["HospitalCode"]."'>";
-      echo $row["HospitalName"];
+      echo $row["HospitalName"] . ", " . $row["City"] . ", " . $row["Province"];
       echo "</option>";
    	}
    	mysqli_free_result($result);
