c6f0b8c97a4dc8bc0668eb988c0b14bdb586a9dc
Christine Richards <cricha88@uwo.ca>
2019-11-18 00:31:24 -0500

    Try delete doctor

--- getdoctorstodel.php
+++ getdoctorstodel.php
@@ -1,12 +1,12 @@
 <?php
-    $query = "SELECT * FROM doctor INNER JOIN hospital ON doctor.HospitalCode=hospital.HospitalCode";
+    $query = "SELECT FirstName, LastName, doctor.LicenseNumber, Specialty, hospital.HospitalName FROM doctor INNER JOIN hospital ON doctor.HospitalCode=hospital.HospitalCode";
     $result = mysqli_query($connection,$query);
     if (!$result) {
         die("databases query for get doctor for delete failed.");
     }
     while ($row = mysqli_fetch_assoc($result)) {
       echo "<option value='".$row["doctor.LicenseNumber"]."'>";
-      echo $row["FirstName"] . ", " . $row["LastName"] . ", " . $row["doctor.LicenseNumber"]. ", " . $row["Specialty"] . ", " . $row["HospitalName"];
+      echo $row["FirstName"] . ", " . $row["LastName"] . ", " . $row["LicenseNumber"]. ", " . $row["Specialty"] . ", " . $row["HospitalName"];
       echo "</option>";
    	}
    	mysqli_free_result($result);
