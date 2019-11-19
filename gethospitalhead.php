9a9a75364639ea96d240d9c14827b93a29a76dcc
Christine Richards <cricha88@uwo.ca>
2019-11-19 03:59:31 -0500

    Hospital Head List To Table

--- gethospitalhead.php
+++ gethospitalhead.php
@@ -4,13 +4,12 @@
     if (!$result) {
       die("databases query on hospital head failed. ");
     }
-    echo "<ul>";
+    echo "<table class='table table-hover'>";
+    echo "<th>Hospital Name</th><th>First Name</th><th>Last Name</th><th>Start Date</th>"; 
     while($row = mysqli_fetch_assoc($result)){
-      echo "<li>";
-      echo $row["HospitalName"] . " - Head: " . $row["FirstName"] . ", " . $row["LastName"] . " - Start Date: " . $row["StartDate"];
-      echo "</li>";
+      echo "<tr><td>" . $row["HospitalName"] . "</td><td>" . $row["FirstName"] . "</td><td>" . $row["LastName"] . "</td><td>" . $row["StartDate"] . "</td></tr>"; 
     }
-    echo "</ul>";
+    echo "</table>";
 
     mysqli_free_result($result);
 ?> 
