24803a5220be91855a97265a7b8aa06ef25d4fb5
Christine Richards <cricha88@uwo.ca>
2019-11-19 03:54:35 -0500

    Hospital Head List All

--- gethospitalhead.php
+++ gethospitalhead.php
@@ -1,5 +1,5 @@
 <?php
-    $query = "SELECT * FROM hospital ORDER BY HospitalName" . ";";
+    $query = "SELECT HospitalName, FirstName, LastName, StartDate FROM hospital INNER JOIN doctor ON hospital.LicenseNumber=doctor.LicenseNumber ORDER BY HospitalName" . ";";
     $result = mysqli_query($connection, $query);
     if (!$result) {
       die("databases query on hospital head failed. ");
@@ -7,7 +7,7 @@
     echo "<ul>";
     while($row = mysqli_fetch_assoc($result)){
       echo "<li>";
-      echo $row["HospitalName"] . ", " . $row["HospitalName"];
+      echo $row["HospitalName"] . " - Head: " . $row["FirstName"] . ", " . $row["LastName"] . " - Start Date: " . $row["StartDate"];
       echo "</li>";
     }
     echo "</ul>";
