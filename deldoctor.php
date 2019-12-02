ab417cabf4c4bcedb671040551ce696493eeff92
Christine Richards <cricha88@uwo.ca>
2019-12-01 20:56:33 -0500

    Update delete doctor

--- deldoctor.php
+++ deldoctor.php
@@ -4,7 +4,7 @@
     $query = 'DELETE FROM doctor WHERE LicenseNumber="' . $deldoc . '"';
 
 	if (!mysqli_query($connection, $query)) {
-		echo "This doctor has patients, please confirm to delete: "
+		echo "This doctor has patients, please confirm to delete: ";
 		echo "<input type='radio' name='confirmdeldoc' value='" . $deldoc . "'> Confirm Deletion <br/>";
 		echo "<input type='radio' name='confirmdeldoc' value=''> Cancel Deletion <br/>";
 		echo "<input type='submit' value='Submit'>'";
