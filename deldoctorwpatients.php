0c35e4cb522e01191237a6591a11234006422f98
Christine Richards <cricha88@uwo.ca>
2019-12-01 21:11:13 -0500

    Update delete doctor

--- deldoctorwpatients.php
+++ deldoctorwpatients.php
@@ -1,13 +1,13 @@
 <?php
 	include 'connectdb.php';
 	$whichdeldoc = $_POST["confirmdeldoc"];
-	if ($whichdeldoc == 9999) {
-		echo "Did not delete doctor.";
-		exit;
-	}
     $query = 'DELETE FROM doctor WHERE LicenseNumber="' . $whichdeldeldoc . '"';
 
-	if (!mysqli_query($connection, $query)) {
+	if ($whichdeldoc == 9999) {
+		echo "Did not delete doctor.";
+		break;
+	}
+	else if (!mysqli_query($connection, $query)) {
 		echo "Error while trying to delete doctor: ". mysqli_error($connection);
 	}
 	else {
