a6acb663f9efa7cedbb03d031b5ff9ce8a793ab2
Christine Richards <cricha88@uwo.ca>
2019-12-01 19:57:22 -0500

    Testing adding error better

--- adddoctor.php
+++ adddoctor.php
@@ -1,7 +1,6 @@
 <?php
 	include 'connectdb.php';
-	print_r($_POST);
-	
+
 	$fname = $_POST["FirstName"];
 	$lname = $_POST["LastName"];
 	$licnum = $_POST["LicenseNumber"];
@@ -12,9 +11,7 @@
 	$licnum .'","' . $fname . '","' . $lname . '","' . $spec . '","' . $licdate . '","' . $hospcode . '");';
 
 	if (!mysqli_query($connection, $query)) {
-		echo "Error on adding new doctor, please enter all fields correctly <br/>";
-		die();
-	} else {
-		exit;
+		echo "Error on adding new doctor, please enter fields correctly: " . mysqli_error($connection) . "<br/>";
 	}
+	
 ?>
