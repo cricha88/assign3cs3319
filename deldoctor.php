16ebbcaf79ef3a1acff151135e30d2036ae5c653
Christine Richards <cricha88@uwo.ca>
2019-12-01 20:54:22 -0500

    Update delete doctor

--- deldoctor.php
+++ deldoctor.php
@@ -5,12 +5,9 @@
 
 	if (!mysqli_query($connection, $query)) {
 		echo "This doctor has patients, please confirm to delete: "
-		echo '<input type="radio" name="confirmdeldoc" value=' . $deldoc . '> Confirm Deletion <br/>';
-		echo '<input type="radio" name="confirmdeldoc" value=''> Cancel Deletion <br/>';
-		echo '<input type="submit" value="Submit">';
-
-		echo "<br/><br/>"
-		echo "Error while trying to delete doctor: " . mysqli_error($connection) . "<br/>";
+		echo "<input type='radio' name='confirmdeldoc' value='" . $deldoc . "'> Confirm Deletion <br/>";
+		echo "<input type='radio' name='confirmdeldoc' value=''> Cancel Deletion <br/>";
+		echo "<input type='submit' value='Submit'>'";
 	}
 	else {
 		echo "Success: deleted doctor.";
